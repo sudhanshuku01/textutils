61db37da11332cebbd5364a7f179b4177f9972a4
Sudhanshu Kumar <sudhanshu620026@gmail.com>
2023-04-19 19:20:34 +0530

    Updates

---
+++ precache-manifest.2a0543098f707eb4eddadf75331cf80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8d32011a471e753dbea",
+    "url": "/textutils/static/css/main.d8eeb8f4.chunk.css"
+  },
+  {
+    "revision": "c8d32011a471e753dbea",
+    "url": "/textutils/static/js/main.c8d32011.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/textutils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "839885bdb393db6476ee",
+    "url": "/textutils/static/js/runtime~main.839885bd.js"
+  },
+  {
+    "revision": "c68e5045411c4b03f0d1e9cb5659b4b8",
+    "url": "/textutils/index.html"
+  }
+];
